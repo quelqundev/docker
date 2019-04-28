214581adb95d637481ee10584917445afeb520ae
quelqun007 <quelqun007@gmail.com>
2019-04-28 16:16:46 +0200

    Modify repositories links

--- updatemanifest.sh
+++ updatemanifest.sh
@@ -1,5 +1,5 @@
-docker manifest create kerberos/kerberos kerberos/kerberos:linux-amd64 kerberos/kerberos:linux-armv7 kerberos/kerberos:linux-armv8 --amend
-docker manifest annotate kerberos/kerberos kerberos/kerberos:linux-armv7 --os linux --arch arm --variant armv7
-docker manifest annotate kerberos/kerberos kerberos/kerberos:linux-armv8 --os linux --arch arm64 --variant armv8
-docker manifest inspect kerberos/kerberos
-docker manifest push kerberos/kerberos --purge
+docker manifest create quelqundev/kerberos quelqundev/kerberos:linux-amd64 quelqundev/kerberos:linux-armv7 quelqundev/kerberos:linux-armv8 --amend
+docker manifest annotate quelqundev/kerberos quelqundev/kerberos:linux-armv7 --os linux --arch arm --variant armv7
+docker manifest annotate quelqundev/kerberos quelqundev/kerberos:linux-armv8 --os linux --arch arm64 --variant armv8
+docker manifest inspect quelqundev/kerberos
+docker manifest push quelqundev/kerberos --purge
